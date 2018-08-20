602f518b401f18ff871cf7bf22a6cf29089ecbf8
stannous2 <stannous2@yahoo.com>
2018-08-19 21:02:11 -0700

    first commit

---
+++ db/seed.sql
@@ -0,0 +1,9 @@
+-- seed file
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES
+('crispy chicken', false),
+('BBQ chicken', false),
+('spicy chicken', false),
+('BBQ beef', false),
+('spicy beef', false);
