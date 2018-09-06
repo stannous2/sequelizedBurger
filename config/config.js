a617e54e7b0cdfa6a7575c5192f2b1a21aee8207
stannous2 <stannous2@yahoo.com>
2018-09-06 07:57:22 -0700

    update config.js

--- config/config.js
+++ config/config.js
@@ -1,23 +1,16 @@
 module.exports = {
   "development": {
-    "username": 'root',
-    "password": 'Asdf1234!',
-    "database": "burgers_db",
-    "host": "localhost",
-    "dialect": "mysql"
-  },
-  "test": {
-<<<<<<< HEAD
     "username": process.env.MYSQL_USER,
     "password": process.env.MYSQL_KEY,
     "database": process.env.MYSQL_DBNAME,
     "host": process.env.MYSQL_HOST,
-=======
-    "username": "root",
-    "password": "Asdf1234!",
-    "database": "burgers_db",
-    "host": "127.0.0.1",
->>>>>>> 33d9b84bb531168587baf93eae3068eb3562c94d
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_KEY,
+    "database": process.env.MYSQL_DBNAME,
+    "host": process.env.MYSQL_HOST,
     "dialect": "mysql"
   },
   "production": {
